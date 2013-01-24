cdcdf5e6ecc63312b625d5b25476622554ec3b8c
Greg Clark <gclark916@ucla.edu>
2013-01-24 13:59:05 -0800

    initial commit

---
+++ Howler/Control/MainController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Gtk;
+using Howler.Gui;
+
+namespace Howler.Control
+{
+    class MainController
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            MainWindow win = new MainWindow();
+            win.Show();
+            Application.Run();
+        }
+    }
+}
---
+++ Howler/Gui/MainWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Howler.Gui
+{
+    public partial class MainWindow : Gtk.Window
+    {
+        public MainWindow() : base(Gtk.WindowType.Toplevel)
+        {
+            this.Name = "MainWindow";
+            this.WindowPosition = ((global::Gtk.WindowPosition)(4));
+            if ((this.Child != null))
+            {
+                this.Child.ShowAll();
+            }
+            this.DefaultWidth = 400;
+            this.DefaultHeight = 300;
+            this.Show();
+        }
+    }
+}
